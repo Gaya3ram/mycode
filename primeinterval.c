6a3915208e99a1a0d2a07bf443b21dd784137b2e
Gaya3ram <30343436+Gaya3ram@users.noreply.github.com>
2017-10-04 15:53:48 +0530

    primeinterval.c

---
+++ primeinterval.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int i,flag,min,max;
+printf("enter the two intervels: ", min,max);
+scanf("%d %d",&min,&max);
+printf("prime number between % and & are, min,max);
+while(min<max)
+flag=0;
+for(i=0;i<low/2;++i)
+{
+if(min%2==0)
+flag==0;
+break;
+}
+if(flag==0)
+printf("%d",min);
+++low;
+}
+return 0;
+}
+
