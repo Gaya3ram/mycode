f451a10ae29a6d3b4db9055ef39f7e72b27361f5
Gaya3ram <30343436+Gaya3ram@users.noreply.github.com>
2017-10-13 13:52:16 +0530

    armbt.c

---
+++ armbt.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int n=0,result=0,high,low,rem;
+printf("enter the two numbers");
+scanf("%d %d ,&low,&high);
+printf("the armstrong number between %d and %d are",low,high);
+for(i=low+1;i<high;i++)
+{
+temp2=i;
+temp1=i:
+}
+while(temp1!=0)
+{temp1=n/10;
+n++;
+}while(temp2!=0)
+{
+temp2=rem%10;
+result=pow(rem,n);
+temp2=n/10;
+}n=0;
+result=0;
+}
