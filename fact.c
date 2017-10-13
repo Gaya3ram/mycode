fecc275a388e1ac1143699f95ee22f84940ee5ac
Gaya3ram <30343436+Gaya3ram@users.noreply.github.com>
2017-10-13 14:09:16 +0530

    fact.c

---
+++ fact.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n,fact=1,i;
+printf("enter the number"0;
+scanf("%d",&n);
+if(n<0)
+printf("error");
+else
+for(i=1;i<=n;i++);
+{
+fact=fact*i;
+}
+printf("the factorial of %d",n,fact);
+return 0;
+}
